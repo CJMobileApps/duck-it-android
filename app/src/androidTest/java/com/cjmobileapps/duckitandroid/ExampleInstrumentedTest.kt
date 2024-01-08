87dbe43145863ed5a493bf7995050cedc72fa8c6
Chris Jones <chris@cjmobileapps.com>
2024-01-07 21:43:25 -0500

    Fix assembleDebug

--- app/src/androidTest/java/com/cjmobileapps/duckitandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/cjmobileapps/duckitandroid/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.cjmobileapps.duckitandroid", appContext.packageName)
+        assertEquals("com.cjmobileapps.duckitandroid.debug", appContext.packageName)
     }
 }
