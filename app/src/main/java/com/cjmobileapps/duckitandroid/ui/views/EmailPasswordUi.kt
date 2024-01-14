f8105f152579d304c4b2dba50393137640adafb4
Chris Jones <chris@cjmobileapps.com>
2024-01-14 16:50:00 -0500

    Update Outlined Password EditText field

--- app/src/main/java/com/cjmobileapps/duckitandroid/ui/views/EmailPasswordUi.kt
+++ app/src/main/java/com/cjmobileapps/duckitandroid/ui/views/EmailPasswordUi.kt
@@ -4,20 +4,31 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.cjmobileapps.duckitandroid.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EmailPasswordUi(
     modifier: Modifier,
@@ -29,6 +40,8 @@ fun EmailPasswordUi(
     isLogInButtonEnabled: Boolean,
     loginButtonClicked: () -> Unit
 ) {
+    var isPasswordVisible by rememberSaveable { mutableStateOf(false) }
+
     Column(modifier = modifier.fillMaxWidth()) {
         //todo add is loading
         if (false) {
@@ -51,14 +64,27 @@ fun EmailPasswordUi(
                 label = { Text(stringResource(R.string.email)) }
             )
 
-            //todo use password outline textfield input
             OutlinedTextField(
                 modifier = Modifier
                     .fillMaxWidth()
                     .testTag(""),
                 value = passwordText,
                 onValueChange = onPasswordValueChange,
-                label = { Text(stringResource(R.string.password)) }
+                label = { Text(stringResource(R.string.password)) },
+                singleLine = true,
+                visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                trailingIcon = {
+                    val image = if (isPasswordVisible) Icons.Filled.Visibility
+                    else Icons.Filled.VisibilityOff
+
+                    val description = if (isPasswordVisible) stringResource(R.string.hide_password)
+                    else stringResource(R.string.show_password)
+
+                    IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                        Icon(imageVector = image, description)
+                    }
+                }
             )
 
             Button(
