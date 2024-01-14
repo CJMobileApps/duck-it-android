de685b138fb1ee365ef28fe55a5e451829e1487f
Chris Jones <chris@cjmobileapps.com>
2024-01-14 16:53:57 -0500

    Fix missing StringConstants not git tracked

---
+++ app/src/main/java/com/cjmobileapps/duckitandroid/data/StringConstants.kt
@@ -0,0 +1,8 @@
+package com.cjmobileapps.duckitandroid.data
+
+object StringConstants {
+    const val passwordIncorrect = "Password Incorrect"
+    const val accountNotFoundTryToCreateAccount = "Account Not Found trying to create account"
+    const val accountAlreadyExists = "Account Already Exists"
+    const val accountNotLoggedIn = "Account Not Logged In"
+}
