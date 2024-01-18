27c6f3bd5dfd42111621d6dc08dbe60a4b9cb7e3
Chris Jones <chris@cjmobileapps.com>
2024-01-18 03:13:25 -0500

    Update LoginViewModel

--- app/src/test/java/com/cjmobileapps/duckitandroid/ui/login/viewmodel/LogInViewModelTest.kt
+++ app/src/test/java/com/cjmobileapps/duckitandroid/ui/login/viewmodel/LogInViewModelTest.kt
@@ -170,4 +170,49 @@ class LogInViewModelTest : BaseTest() {
         Assertions.assertTrue(snackbarState is LogInViewModelImpl.LoginSnackbarState.ShowGenericError)
         Assertions.assertFalse(logInNavRouteUiState is LogInViewModelImpl.LogInNavRouteUi.GoToListScreenUi)
     }
+    @Test
+    fun `enable logInButtonEnabled then loginButtonClicked accountSignedIn then resetSnackbarState & resetNavRouteUiToIdle(`(): Unit = runTest {
+
+        // then
+        setupLogInViewModel()
+        logInViewModel.updateEmailEditText(MockData.mockEmailPasswordRequest.email)
+        logInViewModel.updatePasswordEditText(MockData.mockEmailPasswordRequest.password)
+
+        val email = logInViewModel.getEmailEditText()
+        val password = logInViewModel.getPasswordEditText()
+
+        // verify
+        Assertions.assertTrue(logInViewModel.isLogInButtonEnabled())
+        Assertions.assertEquals(
+            MockData.mockEmailPasswordRequest.email,
+            email
+        )
+        Assertions.assertEquals(
+            MockData.mockEmailPasswordRequest.password,
+            password
+        )
+
+        // when
+        Mockito.`when`(mockAccountUseCase.signIn(MockData.mockEmailPasswordRequest)).thenReturn(MockData.mockAccountStateAccountSignedInResponseWrapper)
+
+        // then
+        logInViewModel.loginButtonClicked()
+        var snackbarState = logInViewModel.getSnackbarState()
+        var logInNavRouteUiState = logInViewModel.getLogInNavRouteUiState()
+
+        // verify
+        Assertions.assertFalse(logInViewModel.isLoading())
+        Assertions.assertTrue(snackbarState is LogInViewModelImpl.LoginSnackbarState.AccountSignedIn)
+        Assertions.assertTrue(logInNavRouteUiState is LogInViewModelImpl.LogInNavRouteUi.GoToListScreenUi)
+
+        // then reset
+        logInViewModel.resetSnackbarState()
+        logInViewModel.resetNavRouteUiToIdle()
+        snackbarState = logInViewModel.getSnackbarState()
+        logInNavRouteUiState = logInViewModel.getLogInNavRouteUiState()
+
+        // verify
+        Assertions.assertTrue(snackbarState is LogInViewModelImpl.LoginSnackbarState.Idle)
+        Assertions.assertTrue(logInNavRouteUiState is LogInViewModelImpl.LogInNavRouteUi.Idle)
+    }
 }
