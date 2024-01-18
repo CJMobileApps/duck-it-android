699ea255b377e05474eb41432e3f8f874062d74e
Chris Jones <chris@cjmobileapps.com>
2024-01-18 01:31:36 -0500

    Add happy flow

--- app/src/test/java/com/cjmobileapps/duckitandroid/ui/login/viewmodel/LogInViewModelTest.kt
+++ app/src/test/java/com/cjmobileapps/duckitandroid/ui/login/viewmodel/LogInViewModelTest.kt
@@ -1,10 +1,72 @@
 package com.cjmobileapps.duckitandroid.ui.login.viewmodel
 
+import com.cjmobileapps.duckitandroid.data.MockData
+import com.cjmobileapps.duckitandroid.data.account.AccountUseCase
 import com.cjmobileapps.duckitandroid.testutil.BaseTest
+import com.cjmobileapps.duckitandroid.testutil.TestCoroutineDispatchers
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.Mockito
 
 class LogInViewModelTest : BaseTest() {
 
-    @Test
+    private lateinit var logInViewModel: LogInViewModel
 
+    @Mock
+    private lateinit var mockAccountUseCase: AccountUseCase
+
+    private fun setupLogInViewModel() {
+        logInViewModel = LogInViewModelImpl(
+            coroutineDispatchers = TestCoroutineDispatchers,
+            accountUseCase = mockAccountUseCase
+        )
+    }
+
+    @Test
+    fun `init logInButtonEnabled not enabled flow`() {
+
+        // then
+        setupLogInViewModel()
+
+        // verify
+        Assertions.assertFalse(logInViewModel.isLogInButtonEnabled())
+    }
+
+    @Test
+    fun `enable logInButtonEnabled then loginButtonClicked accountCreated`(): Unit = runTest {
+
+        // then
+        setupLogInViewModel()
+        logInViewModel.updateEmailEditText(MockData.mockEmailPasswordRequest.email)
+        logInViewModel.updatePasswordEditText(MockData.mockEmailPasswordRequest.password)
+
+        val email = logInViewModel.getEmailEditText()
+        val password = logInViewModel.getPasswordEditText()
+
+        // verify
+        Assertions.assertTrue(logInViewModel.isLogInButtonEnabled())
+        Assertions.assertEquals(
+            MockData.mockEmailPasswordRequest.email,
+            email
+        )
+        Assertions.assertEquals(
+            MockData.mockEmailPasswordRequest.password,
+            password
+        )
+
+        // when
+        Mockito.`when`(mockAccountUseCase.signIn(MockData.mockEmailPasswordRequest)).thenReturn(MockData.mockAccountStateAccountCreatedResponseWrapper)
+
+        // then
+        logInViewModel.loginButtonClicked()
+        val snackbarState = logInViewModel.getSnackbarState()
+        val logInNavRouteUiState = logInViewModel.getLogInNavRouteUiState()
+
+        // verify
+        Assertions.assertFalse(logInViewModel.isLoading())
+        Assertions.assertTrue(snackbarState is LogInViewModelImpl.LoginSnackbarState.AccountCreated)
+        Assertions.assertTrue(logInNavRouteUiState is LogInViewModelImpl.LogInNavRouteUi.GoToListScreenUi)
+    }
 }
